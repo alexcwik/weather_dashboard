cf412be7b801124117be30acaac93b93e0365f20
Alex Cwik <alexcwik1490@gmail.com>
2021-10-11 21:13:21 -0500

    not much here to look at, couldn't get it working

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+let weather = {
+  apiKey: "7e8760c83edf089b61532dce12266d25",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => {
+        if (!response.ok) {
+          alert("No weather found.");
